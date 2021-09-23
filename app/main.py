b7fec0fb98b261a498c52b457ba86bc3abfeaed2
Andres  Niño <30846266+nelsNio@users.noreply.github.com>
2021-09-23 17:46:49 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,12 +23,12 @@ def get_db():
     finally:
         db.close()
 
-origins = ['*'
+origins = ['*','http://localhost:4200'
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
